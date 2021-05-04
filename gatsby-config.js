2327d49b90e40de8693f47902ee5268d7a5235b4
Zack Spellman <zandaleph@gmail.com>
2021-05-03 22:54:42 -0700

    update pixel id

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         facebookPixel: {
-          pixelId: "497061361661150",
+          pixelId: "270030241538729",
           cookieName: "cookie-consent",
         },
         environments: ["production", "development"],
